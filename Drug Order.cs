fc0a5baea4842889b1da1cb15f95b16e57c11386
Thinadhi <thinadhirathnaweera@gmail.com>
2024-09-21 13:01:55 +0530

    new pagers

---
+++ Drug Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inventoryManagement
+{
+    public partial class Drug_Order : Form
+    {
+        public Drug_Order()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Drug_Order_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
