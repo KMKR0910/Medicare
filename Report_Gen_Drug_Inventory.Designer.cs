be274bfae2321e4ef7ed0a7e8e9655b2de5ac3f2
KMKR0910 <kasunrashmika5@gmail.com>
2024-10-06 05:16:29 +0530

    Update report2

---
+++ Report_Gen_Drug_Inventory.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Diploma_Final_Project_1
+{
+    partial class Report_Gen_Drug_Inventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Report_Gen_Drug_Inventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Report_Gen_Drug_Inventory";
+            this.Text = "Report_Gen_Drug_Inventory";
+            this.Load += new System.EventHandler(this.Report_Gen_Drug_Inventory_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
