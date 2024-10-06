837df55c9690c9e477cc96b0556a000142e56dbb
KMKR0910 <kasunrashmika5@gmail.com>
2024-10-06 21:29:38 +0530

    update pha

--- Pha Add Drug Payment.Designer.cs
+++ Pha Add Drug Payment.Designer.cs
@@ -29,12 +29,233 @@ namespace Diploma_Final_Project_1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txt_paymnet_number = new System.Windows.Forms.TextBox();
+            this.txt_patient = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_genarate = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.numericUpDownCost = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_date = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.txt_search = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCost)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1276, 289);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1064, 1074);
+            this.dataGridView1.TabIndex = 110;
+            // 
+            // txt_paymnet_number
+            // 
+            this.txt_paymnet_number.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_paymnet_number.Location = new System.Drawing.Point(1167, 63);
+            this.txt_paymnet_number.Name = "txt_paymnet_number";
+            this.txt_paymnet_number.Size = new System.Drawing.Size(372, 44);
+            this.txt_paymnet_number.TabIndex = 109;
+            // 
+            // txt_patient
+            // 
+            this.txt_patient.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_patient.Location = new System.Drawing.Point(583, 307);
+            this.txt_patient.Name = "txt_patient";
+            this.txt_patient.Size = new System.Drawing.Size(372, 44);
+            this.txt_patient.TabIndex = 108;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(195, 309);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(264, 42);
+            this.label4.TabIndex = 107;
+            this.label4.Text = "Supplier Name";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btn_genarate
+            // 
+            this.btn_genarate.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_genarate.Location = new System.Drawing.Point(520, 1095);
+            this.btn_genarate.Name = "btn_genarate";
+            this.btn_genarate.Size = new System.Drawing.Size(212, 129);
+            this.btn_genarate.TabIndex = 106;
+            this.btn_genarate.Text = "Genarate Bil Invoice";
+            this.btn_genarate.UseVisualStyleBackColor = true;
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clear.Location = new System.Drawing.Point(520, 943);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(216, 90);
+            this.btn_clear.TabIndex = 105;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.Location = new System.Drawing.Point(520, 780);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(216, 90);
+            this.btn_add.TabIndex = 104;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // numericUpDownCost
+            // 
+            this.numericUpDownCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDownCost.Location = new System.Drawing.Point(583, 630);
+            this.numericUpDownCost.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDownCost.Name = "numericUpDownCost";
+            this.numericUpDownCost.Size = new System.Drawing.Size(372, 44);
+            this.numericUpDownCost.TabIndex = 103;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(195, 617);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 42);
+            this.label2.TabIndex = 102;
+            this.label2.Text = "Total Cost";
+            // 
+            // txt_date
+            // 
+            this.txt_date.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_date.Location = new System.Drawing.Point(583, 522);
+            this.txt_date.Name = "txt_date";
+            this.txt_date.Size = new System.Drawing.Size(372, 44);
+            this.txt_date.TabIndex = 101;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(195, 519);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 42);
+            this.label1.TabIndex = 100;
+            this.label1.Text = "Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(195, 426);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(600, 84);
+            this.label3.TabIndex = 98;
+            this.label3.Text = "Payment Method";
+            // 
+            // btn_search
+            // 
+            this.btn_search.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_search.Location = new System.Drawing.Point(894, 52);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(216, 55);
+            this.btn_search.TabIndex = 95;
+            this.btn_search.Text = "Search";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // txt_search
+            // 
+            this.txt_search.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_search.Location = new System.Drawing.Point(441, 58);
+            this.txt_search.Name = "txt_search";
+            this.txt_search.Size = new System.Drawing.Size(372, 44);
+            this.txt_search.TabIndex = 96;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(115, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(568, 84);
+            this.label5.TabIndex = 97;
+            this.label5.Text = "Search Supplier";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Cash",
+            "Card"});
+            this.comboBox1.Location = new System.Drawing.Point(583, 426);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(372, 45);
+            this.comboBox1.TabIndex = 111;
+            // 
+            // Pha_Add_Drug_Payment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(2427, 1449);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txt_paymnet_number);
+            this.Controls.Add(this.txt_patient);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_genarate);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.numericUpDownCost);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_date);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.txt_search);
+            this.Controls.Add(this.label5);
+            this.Name = "Pha_Add_Drug_Payment";
             this.Text = "Pha_Add_Drug_Payment";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCost)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txt_paymnet_number;
+        private System.Windows.Forms.TextBox txt_patient;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_genarate;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.NumericUpDown numericUpDownCost;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_date;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.TextBox txt_search;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- Pha Add Drug Payment.cs
+++ Pha Add Drug Payment.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Diploma_Final_Project_1
 {
@@ -15,6 +16,90 @@ namespace Diploma_Final_Project_1
         public Pha_Add_Drug_Payment()
         {
             InitializeComponent();
+        }
+        String supplierID;
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            string cs = "Data Source=ASUS; Initial Catalog =Diploma Final Project DB1; Integrated Security=True";
+
+            try
+            {
+                SqlConnection con = new SqlConnection(cs);
+                con.Open();
+
+
+                string query = "INSERT INTO tbl_drug_payments (Pay_Date, Payment_Method, Total_Cost, Suppler_ID)VALUES(@Pay_Date, @Payment_Method, @Total_Cost, @Suppler_ID)";
+                
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue("@Pay_Date", txt_date.Text);
+                cmd.Parameters.AddWithValue("@Payment_Method", comboBox1.SelectedItem);
+                cmd.Parameters.AddWithValue("@Total_Cost", numericUpDownCost.Value);
+                cmd.Parameters.AddWithValue("@Suppler_ID", supplierID);
+
+
+                int ret = cmd.ExecuteNonQuery();
+                if (ret > 0)
+                {
+                    MessageBox.Show("Added successfully");
+                    txt_date.Clear();
+                    comboBox1.SelectedIndex=-1;
+                    
+                 
+                    numericUpDownCost.Value = 0;
+
+                }
+
+                con.Close();
+                MessageBox.Show("Payment added ");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred : " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            try
+            {
+
+
+
+                SqlConnection con = new SqlConnection(cs);
+                con.Open();
+
+
+
+
+
+                string sql = @"
+            SELECT p.Name, td.Patient_pay_ID,td.[Payment Type], td.[Total_Cost] ,td.[Date] 
+            FROM [tbl_Patient_Payment] td
+            INNER JOIN tbl_patient_info p ON td.[patirnt_ID] = p.[Patient ID]
+            WHERE p.[Contact Number] = @name ";
+                SqlCommand com = new SqlCommand(sql, con);
+
+                com.Parameters.AddWithValue("@name", this.txt_search.Text);
+
+
+
+                SqlDataAdapter dap = new SqlDataAdapter(com);
+                DataSet ds = new DataSet();
+                dap.Fill(ds);
+
+                this.dataGridView1.DataSource = ds.Tables[0];
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred : " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            DateTime currentDate = DateTime.Now.Date;
+            this.txt_date.Text = currentDate.ToString("yyyy-MM-dd");
         }
     }
 }
